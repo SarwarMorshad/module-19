885176ca18fcdeebdb81079597f30d0e1954243d
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-09 01:27:00 +0200

    First Function

---
+++ function.js
@@ -0,0 +1,15 @@
+// !! How to write a function in JavaScript !!
+/*
+ * function name
+ * function parameters
+ * function body
+ * function return value
+ */
+
+function add(a, b) {
+  // function body
+  return a + b; // function return value
+}
+
+add(2, 3); // function call
+console.log(add(2, 3)); // Output: 5
