c36901b0905088615a484ba6dc9e6fb704466ca5
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-10 22:14:06 +0200

    Practicing Funtion

--- function.js
+++ function.js
@@ -23,3 +23,16 @@ function sumArray(arr) {
 const numbers = [1, 2, 3, 4, 5];
 const sum = sumArray(numbers);
 console.log(sum);
+
+function even(arr) {
+  const evenNumbers = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenNumbers.push(arr[i]);
+    }
+  }
+  return evenNumbers; // function return value
+}
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const evenNumbers = even(numbersArray);
+console.log(evenNumbers); // Output: [2, 4, 6, 8]
